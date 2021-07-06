ca05d69f55113d64aa691f2b247fb26f70340f67
Piero <piero.onorati@virgilio.it>
2021-07-06 15:53:15 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header/>
+    <!-- <MainContent/>
+    <HighlightedContent/>
+    <Footer/>
+    <CTASocial/>   -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+// import MainContent from './components/MainContent.vue'
+// import HighlightedContent from './components/HighlightedContent.vue'
+// import Footer from './components/Footer.vue'
+// import CTASocial from './components/CTASocial.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Header,
+    // MainContent,
+    // HighlightedContent,
+    // Footer,
+    // CTASocial
   }
 }
 </script>
